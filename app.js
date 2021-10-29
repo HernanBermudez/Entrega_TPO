2b6e71726f36b27e81b348c0646067f02a00166a
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2021-10-29 14:20:51 -0500

    Terminado - Limpieza de codigo
    Finalización, entrega

--- app.js
+++ app.js
@@ -1,39 +1,4 @@
 
-function clickCounter() {
-    if(typeof(Storage) !== "undefined") {
-      if (sessionStorage.clickcount) {
-          
-        sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-        displayMenu();
-
-      }else {
-        sessionStorage.clickcount = 1;
-      }  
-    } else {
-      document.getElementById("menu").innerHTML = "Sorry, your browser does not support web storage...";
-    }
-    return sessionStorage.clickcount;
-}
-
-function displayMenu(){
-
-    if(sessionStorage.clickcount % 2 != 0){
-        
-        document.getElementById("menu").style.opacity = 1;
-        
-    }
-    else{
-        
-        document.getElementById("menu").style.opacity = 0;
-    }
-}
-
-function onMouseOver(element, color){
-
-        element.style.color = color;
-
-}
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -64,6 +29,7 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+//Galería
 
 function showSlides(n) {
   var i;
